e54e1a1f2efb75cf40a9b1872b61f762894158c0
Shubhi <joshigarima906@gmail.com>
2026-03-24 18:34:56 +0530

    Initial commit

---
+++ rydex/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "RYDEX - Smart Vehicle Booking Platform",
+  description: "RYDEX is a smart vehicle booking platform that enables users to easily search, compare, and book vehicles with a seamless and efficient experience.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
